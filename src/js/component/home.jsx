2d280305d252eb8b0fe4c5a6a02854c9f776339d
jcrews123 <168486450+jcrews123@users.noreply.github.com>
2024-07-07 23:16:36 +0000

    semi working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,77 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React , {useState} from "react";
+class Reservation {
+	constructor(name, email, seats, service) {
+	  this.name = name;
+	  this.email = email;
+	  this.seats = seats;
+	  this.service = service;
+	}
+  }
+ 
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const [currentReservation,setCurrentReservation] = useState({})
+const [reservationList,setReservationList] = useState([])
+const strUser = e.options[e.selectedIndex].text;
+return (
+	<div className="text-center">
+		<div>
+			<input
+				type="text"
+				onChange={(event) => 
+					currentReservation.name = event.target.value}
+				value={currentReservation.name}
+				placeholder="Full Name">
+			</input>
+			<input
+				type="text"
+				onChange={(event) => 
+					currentReservation.email = event.target.value}
+				value={currentReservation.email}
+				placeholder="Email">
+			</input>
+			<input
+				type="text"
+				onChange={(event) => 
+					currentReservation.email = event.target.value}
+				value={currentReservation.email}
+				placeholder="Email">
+			</input>
+			<div class="input-group mb-3">
+  				<select class="form-select" id="inputGroupSelect01">
+   					<option value="regular">Regular</option>
+    				<option value="vip">Vip</option>
+  				</select>
+			</div>
 		</div>
+		<button 
+			className="button button-secondary" 
+			onClick={(event) => 
+				{
+					reservationList.push(currentReservation)
+					setCurrentReservation(new Reservation ("","","",""))
+				}
+			}>
+			Set Reservation
+		</button>
+		<table>
+  			<tr>
+    			<th>Full Name</th>
+   				<th>Email</th>
+    			<th>Seats</th>
+				<th>Service</th>
+  			</tr>
+				{reservationList.map((eachReservation,index) =>(
+					<tr key={index}>
+						<td>{eachReservation.name}</td>
+						<td>{eachReservation.email}</td>
+						<td>{eachReservation.seats}</td>
+						<td>{strUser}</td>
+					</tr>
+
+					
+				))}
+		</table>
+	</div>
 	);
 };
 
